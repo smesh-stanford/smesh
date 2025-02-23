d02a68c3606a7e0b9aced1c2d669e26aa447915b
pycuber <134294337+pete4radio@users.noreply.github.com>
2025-02-23 10:40:49 -0800

    fixed swinstall path maybe

---
+++ snode/scripts/new_ini_swinstall.sh
@@ -0,0 +1,17 @@
+sudo apt update
+sudo apt -y upgrade
+sudo apt -y install git pip
+
+directory_name="/home/pi/Documents"
+git clone "https://github.com/smesh-stanford/smesh.git" "$directory_name/smesh"
+
+# add the path that Python will be looking for
+grep "PATH" ~/.bashrc | echo "PATH=$PATH:/home/pi/.local/bin" >> ~/.bashrc
+# cd smesh/snode
+source /home/pi/.bashrc
+
+# Install the required packages 
+pip install -r /home/pi/Documents/smesh/snode/requirements.txt --break-system-packages
+
+# Initialize the radio (if connected)
+python -m meshtastic --configure /home/pi/Documents/smesh/firmware/build_1_config.yaml
