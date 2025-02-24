98775bb69cb4b9f82928ef926f29c10849807f77
pycuber <134294337+pete4radio@users.noreply.github.com>
2025-02-24 10:26:22 -0800

    all files in /boot (/firmware not available at image time)

--- snode/scripts/ini_logger.sh
+++ snode/scripts/ini_logger.sh
@@ -3,16 +3,16 @@
 #  For this to work, after imaging the OS from your laptop, do this
 #      git clone https://github.com/smesh-stanford/smesh.git ; cp smesh/snode/script/ini_* <path to sd card>/boot/firmware
 
-sudo chmod +x /boot/firmware/ini_cron.sh
-sudo chmod +x /boot/firmware/ini_hotspot.sh
-sudo chmod +x /boot/firmware/ini_overlays.sh
-sudo chmod +x /boot/firmware/ini_swap.sh
-sudo chmod +x /boot/firmware/ini_swinstall.sh
-sudo chmod +x /boot/firmware/ini_wifi.sh
+sudo chmod +x /boot/ini_cron.sh
+sudo chmod +x /boot/ini_hotspot.sh
+sudo chmod +x /boot/ini_overlays.sh
+sudo chmod +x /boot/ini_swap.sh
+sudo chmod +x /boot/ini_swinstall.sh
+sudo chmod +x /boot/ini_wifi.sh
 
-sudo source /boot/firmware/ini_wifi.sh >> /boot/firstrun_ini_wifi.log 2>&1
-sudo source /boot/firmware/ini_overlays.sh >> /boot/firstrun_ini_overlays.log 2>&1
-sudo source /boot/firmware/ini_swap.sh >> /boot/firstrun_ini_swap.log 2>&1
-sudo source /boot/firmware/ini_swinstall.sh >> /boot/firstrun_ini_swinstall.log 2>&1
-sudo source /boot/firmware/ini_cron.sh >> /boot/firstrun_ini_cron.log 2>&1
-sudo source /boot/firmware/ini_hotspot.sh >> /boot/firstrun_ini_hotspot.log 2>&1
+sudo source /boot/ini_wifi.sh >> /boot/firstrun_ini_wifi.log 2>&1
+sudo source /boot/ini_overlays.sh >> /boot/firstrun_ini_overlays.log 2>&1
+sudo source /boot/ini_swap.sh >> /boot/firstrun_ini_swap.log 2>&1
+sudo source /boot/ini_swinstall.sh >> /boot/firstrun_ini_swinstall.log 2>&1
+sudo source /boot/ini_cron.sh >> /boot/firstrun_ini_cron.log 2>&1
+sudo source /boot/ini_hotspot.sh >> /boot/firstrun_ini_hotspot.log 2>&1
