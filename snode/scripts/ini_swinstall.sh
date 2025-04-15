77b0d63ab8e8aa4b54876a8aec0f499316d812e9
Daniel Neamati <danineamati@gmail.com>
2025-04-14 20:59:54 -0700

    apt-get install not yet working

--- snode/scripts/ini_swinstall.sh
+++ snode/scripts/ini_swinstall.sh
@@ -4,16 +4,20 @@ directory_name="/home/pi/Documents"
 mkdir -p "$directory_name"
 
 # Install the necessary packages
+echo "---"
+echo "Checking internet connection..."
+ping google.com -c 3
+date
 
 # the line below is a bit scary, but it seems to be a possible solution to 
 # apt-get update error (mainly the package lists are out of date and need to 
 # be completely refreshed)
-sudo rm /var/lib/apt/lists/* 
+sudo rm -vrf /var/lib/apt/lists/* 
 # apt-get is the script version. using apt will not work here
 # also, update seems to be needed before install
 sudo apt-get update
 sudo apt-get -y install git pip screen
-# network-manager screen dos2unix
+# sudo apt-get -y install network-manager screen dos2unix
 #sudo apt update
 #sudo apt -y upgrade
 
@@ -39,3 +43,9 @@ sudo -u pi pip install -r /home/pi/Documents/smesh/snode/requirements.txt --brea
 
 # Initialize the radio (if connected) as pi!
 sudo -u pi python -m meshtastic --configure /home/pi/Documents/smesh/firmware/build_1_config.yaml
+
+# Check if the USB is available in devices
+echo "---"
+echo "Checking tty devices (should see a ttyUSB*)..."
+ls /dev/ttyUSB*
+echo "---"
